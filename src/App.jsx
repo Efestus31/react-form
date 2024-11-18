c2f5c5e3c2c5541a183414847a68f528b2256e95
Efestus31 <biagiohd@gmail.com>
2024-11-18 13:52:49 +0100

    add struttura base markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+const articlesTitle = [
+  "Intelligenza Artificiale e Automazione: Come Cambieranno le Professioni nel 2030",
+  "Smartphone Pieghevoli: Tra Innovazione e Realtà del Mercato",
+  "La Sicurezza nel Mondo Digitale: Le Minacce Emergenti del 2024",
+  "5G vs. Wi-Fi 6: Qual è la Tecnologia di Rete del Futuro?",
+  "Blockchain Oltre le Criptovalute: Nuove Frontiere in Settori Inaspettati"
+];
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [titles, setTitles] = useState(articlesTitle)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="container">
+        <h1>Form</h1>
+        <form>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Recipient's username"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+            />
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+          </div>
+        </form>
+
+
+        <ul class="list-group list-group-numbered">
+          {articlesTitle.map((article, index) => <li key={index} className='list-group-item'>
+            {article}
+          </li>)}
+          <li class="list-group-item ">Active item</li>
+
+        </ul>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
